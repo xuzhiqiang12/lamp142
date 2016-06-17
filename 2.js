0e22c6ba0fd0788c54de283468f871ad0e9be071
xuzhiqiang12 <1048781601@qq.com>
2016-06-17 17:07:33 +0800

    1111

---
+++ 2.js
@@ -0,0 +1,10 @@
+/* 
+ * Copyright(c)2016 All rights reserved.
+ * @Licenced  http://www.w3.org
+ * @Author  liutian<1538731090@qq.com> liutian_jiayi
+ * @Create on 2016-5-24 9:04:30
+ * @Version 1.0
+ */
+//在JS文件 无需再写script标签 直接写代码即可
+alert("world");
+
